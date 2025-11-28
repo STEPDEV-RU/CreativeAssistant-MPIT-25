adf38da1b4ea085b49197f5998ad924677212901
STEPDEV <blade-games@mail.ru>
2025-11-28 11:17:26 +1000

    added NSFW filter

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
 import io
 import os
 import re
+import json
 from flask import Flask, request, send_file, jsonify
 import torch
 from diffusers import KandinskyV22Pipeline, KandinskyV22PriorPipeline
 from transformers import MarianTokenizer, MarianMTModel
 from flask_cors import CORS
 
-# app init
+# =============================
+# APP INIT
+# =============================
 app = Flask(__name__)
 CORS(app)
 
-# ============================================
-#   LOAD LOCAL KANDINSKY 2.2 (prior + decoder)
-# ============================================
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# =============================
+# LOAD NSFW WORDS
+# =============================
+
+print("Loading NSFW Filter...")
+
+NSFW_FILE = os.path.join(BASE_DIR, "nsfw.json")
+with open(NSFW_FILE, "r", encoding="utf-8") as f:
+    nsfw_data = json.load(f)
+
+nsfw_words = nsfw_data.get("nsfw_words", [])
+
+def is_safe_prompt(prompt: str):
+    prompt_lower = prompt.lower()
+    return not any(word in prompt_lower for word in nsfw_words)
+
+# =============================
+# LOAD MODELS
+# =============================
 MODEL_DIR = os.path.join(BASE_DIR, "models/kandinsky-2-2")
 TRANSLATE_MODEL_DIR = os.path.join(BASE_DIR, "models/opus-mt-ru-en")
 
@@ -34,26 +53,25 @@ pipe_decoder = KandinskyV22Pipeline.from_pretrained(
 ).to("cuda")
 
 print("Loading OPUS MT RU-EN ...")
-
 translator_tokenizer = MarianTokenizer.from_pretrained(TRANSLATE_MODEL_DIR, local_files_only=True)
 translator_model = MarianMTModel.from_pretrained(TRANSLATE_MODEL_DIR, local_files_only=True)
 
 print("Models loaded successfully!")
 
-# вспомогательная ф-ция для перевода русского промпта
+# =============================
+# HELPER FUNCTIONS
+# =============================
 def translate_ru_to_en(text: str):
     tokens = translator_tokenizer(text, return_tensors="pt", padding=True)
     translated = translator_model.generate(**tokens)
     return translator_tokenizer.batch_decode(translated, skip_special_tokens=True)[0]
 
-# проверка на русский промпт
 def is_russian(text: str):
     return bool(re.search("[А-Яа-яЁё]", text))
 
-# ============================================
-#   API ENDPOINT
-# ============================================
-
+# =============================
+# API ENDPOINT
+# =============================
 @app.route("/generate", methods=["POST"])
 def generate_image():
     data = request.get_json()
@@ -62,7 +80,9 @@ def generate_image():
         return jsonify({"error": "Missing 'prompt'"}), 400
 
     prompt = data["prompt"]
+    negative_prompt = data.get("negative_prompt", "low quality, bad quality")
 
+    # перевод промпта и негативного промпта, если на русском
     if is_russian(prompt):
         prompt_en = translate_ru_to_en(prompt)
         print(f"Detected RU -> EN: {prompt} -> {prompt_en}")
@@ -70,12 +90,15 @@ def generate_image():
         prompt_en = prompt
         print(f"Detected EN: {prompt}")
 
-    negative_prompt = data.get("negative_prompt", "low quality, bad quality")
-
     if is_russian(negative_prompt):
-        print(f"Detected RU negative prompt: {negative_prompt}")
         negative_prompt = translate_ru_to_en(negative_prompt)
         print(f"negative prompt RU -> EN: {negative_prompt}")
+
+    # =============================
+    # NSFW CHECK
+    # =============================
+    if not is_safe_prompt(prompt_en) or not is_safe_prompt(negative_prompt):
+        return jsonify({"error": "Ваш промпт содержит запрещённые запросы. Введите допустимый промпт и попробуйте снова!"}), 400
 
     print(f"Generating image from prompt: {prompt} (EN: {prompt_en})")
 
@@ -90,7 +113,6 @@ def generate_image():
         image = pipe_decoder(
             **prior_out,
             num_inference_steps=20,
-            # prior_guidance_scale=1.0,
             height=512,
             width=512
         ).images[0]
@@ -101,7 +123,6 @@ def generate_image():
         buf.seek(0)
 
         print("Generation OK")
-
         return send_file(buf, mimetype="image/png", as_attachment=True, download_name="result.png")
 
     except Exception as e:
